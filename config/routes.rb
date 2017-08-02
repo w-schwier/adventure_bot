de16e8132e76126510a0a1f38168fd66ce693325
Will Schwier <w.schwier@hotmail.co.uk>
2017-08-02 12:07:05 +0100

    add options MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :game do
-    resources :responses
+    resources :responses do
+      resources :options
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170801171031) do
+ActiveRecord::Schema.define(version: 20170802110410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "options", force: :cascade do |t|
+    t.text "message"
+    t.integer "next_response_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "responses", force: :cascade do |t|
     t.text "message"
